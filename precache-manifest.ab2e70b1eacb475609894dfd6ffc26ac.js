91026edd81b282862147eca9ea8c5437cd3be3e0
ammar <ammarkaan@gmail.com>
2019-11-12 09:45:13 +0500

    first commit

---
+++ precache-manifest.ab2e70b1eacb475609894dfd6ffc26ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3644a4d92226e5fbbfed145f0614dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a76ac86ecbba750c1987",
+    "url": "/static/css/main.49acbacd.chunk.css"
+  },
+  {
+    "revision": "aa375529c0058382948d",
+    "url": "/static/js/2.6c25fee6.chunk.js"
+  },
+  {
+    "revision": "a76ac86ecbba750c1987",
+    "url": "/static/js/main.409f8d57.chunk.js"
+  },
+  {
+    "revision": "825e6eaf4bce2c44d7a0",
+    "url": "/static/js/runtime-main.e74a5878.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
